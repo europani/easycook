229b770a356851f0dbcf933198a2f85bebf49ed1
europani <europani1212@gmail.com>
2020-11-01 10:50:38 +0900

    event coupon ucoupon table

--- src/main/java/com/devon/easycook/domain/CouponDTO.java
+++ src/main/java/com/devon/easycook/domain/CouponDTO.java
@@ -2,20 +2,15 @@ package com.devon.easycook.domain;
 
 import java.sql.Timestamp;
 
-import org.apache.ibatis.type.Alias;
-
 import lombok.Data;
 
 @Data
 public class CouponDTO {
 
 	private int couponNo;
-	private String id;
-	private int couponType;
+	private String couponTitle;
 	private int couponDiscount;
-	private int couponUsed;
 	private Timestamp couponSdate;
 	private Timestamp couponEdate;
-	private String couponTitle;
-	private String couponContent;
+	
 }
---
+++ src/main/webapp/WEB-INF/views/order/cancelSuccess.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<meta http-equiv="refresh" content="0; url=<%=request.getContextPath()%>/mypage/orders"> 
+</head>
+<body>
+<script>
+	alert('반품처리가 완료되었습니다.');
+</script>
+</body>
+</html>
